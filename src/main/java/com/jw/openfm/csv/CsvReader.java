0fae33d61b72b9afa55d0ca5d742a7ac3bee7ca2
Jostein Wiig <jostein.wiig@gmail.com>
2018-08-17 21:03:48 +0200

    Removed unused import

--- src/main/java/com/jw/openfm/csv/CsvReader.java
+++ src/main/java/com/jw/openfm/csv/CsvReader.java
@@ -2,7 +2,6 @@ package com.jw.openfm.csv;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
--- src/main/java/com/jw/openfm/domain/Player.java
+++ src/main/java/com/jw/openfm/domain/Player.java
@@ -9,6 +9,29 @@ public class Player extends Person {
 
     private final Position position;
 
+    /**
+     * Technical attributes:
+     * -  private int finishing;
+     * -  private int technique;
+     * -  private int passing;
+     * -  private int defending;
+     * Mental attributes:
+     * -  private int creativity;
+     * -  private int teamwork;
+     * -  private int determination;
+     * -  private int aggression;
+     * Physical attributes:
+     * -  private int speed;
+     * -  private int jumping;
+     * -  private int strength;
+     * -  private int stamina;
+     * Goalkeeper attributes:
+     * -  private int arialability;
+     * -  private int communication;
+     * -  private int technique;
+     * -  private int reflexes; 
+     */
+
     public Player(String firstname, String lastname, LocalDate birthdate, Position position) {
         super(firstname, lastname, birthdate);
         this.position = position;
